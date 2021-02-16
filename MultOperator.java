8916459b10f70043feabbbbadb828e8d248d9cdb
tay_meow <taytayhedrick@gmail.com>
2021-02-16 14:48:09 -0500

    Started MultOperator & Changed all headings

--- MultOperator.java
+++ MultOperator.java
@@ -1,15 +1,34 @@
 //**************************************************************************************************************
 // CLASS: MultOperator
 //
-// AUTHOR
-// Kevin R. Burger (burgerk@asu.edu)
-// Computer Science & Engineering Program
-// Fulton Schools of Engineering
-// Arizona State University, Tempe, AZ 85287-8809
-// http://www.devlang.com
+// * COURSE AND PROJECT INFORMATION 
+// * CSE205 Object Oriented Programming and Data Structures, Spring 2021
+// * Project Number: p4
+// *
+// * GROUP INFORMATION  
+// * AUTHOR 1: Brandon Murata, bmurata1, bmurata1@asu.edu
+// * AUTHOR 2: Brandon Billmeyer, bbillmey , bbillmey@asu.edu
+// * AUTHOR 3: Delaney Claussen , djclaus1, djclaus1@asu.edu
+// * AUTHOR 4: Taylor Hedrick, tmhedric, tmhedric@asu.edu
 //**************************************************************************************************************
 
 /**
  * Represents the multiplication operator which is a specific type of binary operator.
  */
-???
+public class MultOperator{
+	public MultOperator() {
+		
+	}
+	
+	public Operand evaluate(Operand pLhsOperand, Operand pRhsOperand) {
+		
+	}
+	
+	public int precedence() {
+		
+	}
+	
+	public int stackPrecedence() {
+		
+	}
+}
--- Token.java
+++ Token.java
@@ -1,12 +1,15 @@
 //**************************************************************************************************************
 // CLASS: Token
 //
-// AUTHOR
-// Kevin R. Burger (burgerk@asu.edu)
-// Computer Science & Engineering Program
-// Fulton Schools of Engineering
-// Arizona State University, Tempe, AZ 85287-8809
-// Web: http://www.devlang.com
+// * COURSE AND PROJECT INFORMATION 
+// * CSE205 Object Oriented Programming and Data Structures, Spring 2021
+// * Project Number: p4
+// *
+// * GROUP INFORMATION  
+// * AUTHOR 1: Brandon Murata, bmurata1, bmurata1@asu.edu
+// * AUTHOR 2: Brandon Billmeyer, bbillmey , bbillmey@asu.edu
+// * AUTHOR 3: Delaney Claussen , djclaus1, djclaus1@asu.edu
+// * AUTHOR 4: Taylor Hedrick, tmhedric, tmhedric@asu.edu
 //**************************************************************************************************************
 
 /**
